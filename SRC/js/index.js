ed7526b6a8489b580e0dabd1951209b6620f12bb
gustavo henry da costa alves henry <gustavohen028@gmail.com>
2025-04-08 15:17:12 -0400

    Add files via upload

---
+++ SRC/js/index.js
@@ -0,0 +1,20 @@
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () =>{
+        const botaoselecionado = document.querySelector(".botao.selecionado");
+        botaoselecionado.classList.remove("selecionado");
+
+        botao.classList.add("selecionado");
+      
+       const personagemselecionado = document.querySelector(".personagem.selecionado");
+       personagemselecionado.classList.remove("selecionado");
+
+       personagens[indice].classList.add("selecionado");
+        
+    });
+});
+
+
